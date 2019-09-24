2d1ce07f8adfb0250d0be0c30fb2fd6a9ed8efec
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-09-24 17:43:51 -0400

    import Title Wrapper and .json files to App.js

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import css from './App.css';
 import Wrapper from './components/Wrapper';
+import Title from "./components/Title";
+import cards from "../src/cards.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    cards
+  };
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Card Match</Title>
+        {this.state.cards.map(card => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 function App() {
   return <Wrapper />
