dc669fefea8fe2c7958747948b19212c62f0e20c
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-09-26 20:41:14 -0400

    changed file names for components

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -5,6 +5,7 @@ import Wrapper from './components/Wrapper';
 import Title from "./components/Title";
 import cards from "../src/cards.json";
 import MovieCard from './components/MovieCard';
+const underscore = require("underscore");
 
 class App extends React.Component {
   state = {
@@ -17,37 +18,43 @@ class App extends React.Component {
     })
   }
 
-  shuffle() {
-    let shuffleArray = this.state.cards, temporaryValue, randomIndex;
-
-    while (shuffleArray !== 0) {
-      randomIndex = Math.floor(Math.random() * shuffleArray);
-      shuffleArray = -1;
-      temporaryValue = array[shuffleArray];
-      array[shuffleArray] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
+  shuffle = event => {
+    event.preventDefault();
+    this.state.cards
   }
+}
 
-  //pass a function to the on click listener
-  //images in array
-  render() {
-    return (
-      <Wrapper>
-        <Title>Card Match</Title>
-        {this.state.cards.map(card => (
-          <MovieCard
-            id={card.id}
-            name={card.name}
-            image={card.image}
-            movie={card.movie}
-            shuffle={this.shuffle}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+// shuffle() {
+//   let shuffleArray = this.state.cards, temporaryValue, randomIndex;
+
+//   while (shuffleArray !== 0) {
+//     randomIndex = Math.floor(Math.random() * shuffleArray);
+//     shuffleArray = -1;
+//     temporaryValue = array[shuffleArray];
+//     array[shuffleArray] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
+//   return array;
+// }
+
+//pass a function to the on click listener
+//images in array
+render() {
+  return (
+    <Wrapper>
+      <Title>Card Match</Title>
+      {this.state.cards.map(card => (
+        <MovieCard
+          id={card.id}
+          name={card.name}
+          image={card.image}
+          movie={card.movie}
+          shuffle={this.shuffle}
+        />
+      ))}
+    </Wrapper>
+  );
+}
 }
 
 export default App;
