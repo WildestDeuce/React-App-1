52e16d6588cf6e6dfa3da0062f39aacb76a89b02
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-10-01 22:21:29 -0400

    Updates

---
+++ precache-manifest.257afdccbdd979db42bee57fbd5a5ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa55ffe965897463de678db95463cef7",
+    "url": "/React-App-1/index.html"
+  },
+  {
+    "revision": "55edcf17a3780fbfd8bc",
+    "url": "/React-App-1/static/css/main.e3697a01.chunk.css"
+  },
+  {
+    "revision": "411944c56224e36ed4a2",
+    "url": "/React-App-1/static/js/2.c19ffd75.chunk.js"
+  },
+  {
+    "revision": "55edcf17a3780fbfd8bc",
+    "url": "/React-App-1/static/js/main.47e15254.chunk.js"
+  },
+  {
+    "revision": "843a5480d96946b795c3",
+    "url": "/React-App-1/static/js/runtime-main.fb57990d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/React-App-1/static/media/logo.25bf045c.svg"
+  }
+]);
